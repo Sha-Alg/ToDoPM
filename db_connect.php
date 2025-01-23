e04ef1ac0ab3958b5e2f48f312f1a39e051b36c7
Sha-Alg <midbdiwakf1543@gmail.com>
2025-01-23 03:38:42 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,15 @@
-<?php 
+<?php
 try {
-
-    $conn= new mysqli('todopm.c90aomi2iaab.eu-north-1.rds.amazonaws.com','sharifah','SHailzx&1234','todopmdb')or die("Could not connect to Cloud database".mysqli_error($con));
-
-}catch (Exception $e){
+    // Create a new MySQL connection
+    $conn = new mysqli(
+        'todopm.c90aomi2iaab.eu-north-1.rds.amazonaws.com', // Hostname (AWS RDS endpoint)
+        'sharifah',                                        // Username
+        'SHailzx&1234',                                    // Password
+        'todopmdb'                                         // Database name
+    ) or die("Could not connect to Cloud database" . mysqli_error($con));
+} catch (Exception $e) {
+    // Redirect to an error page if the connection fails
     header('location: 404.html');
 }
+
 
